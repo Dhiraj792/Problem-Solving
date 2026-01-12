26701c6cf6f0f4bbeeab467d429c6ee442a6a13e
Dhiraj792 <dhirajbadre@gmail.com>
2026-01-12 22:18:52 +0530

    Add function to convert sentence to keypad sequence
    Implement function to convert uppercase sentence to mobile keypad sequence.

---
+++ Strings/onvert a sentence into its equivalent mobile numeric keypad sequence.cpp
@@ -0,0 +1,40 @@
+/*
+Given a sentence in the form of a string in uppercase, convert it into its equivalent mobile numeric keypad sequence. Please note there might be spaces in between the words in a sentence and we can print spaces by pressing 0.
+
+Input:
+S = "GFG"
+Output: 43334
+Explanation: For 'G' press '4' one time.
+For 'F' press '3' three times.
+Example 2:
+
+Input:
+S = "HEY U"
+Output: 4433999088
+Explanation: For 'H' press '4' two times.
+For 'E' press '3' two times. For 'Y' press '9' 
+three times. For white space press '0' one time.
+For 'U' press '8' two times.
+*/
+string printSequence(string S) {
+    // code here.
+    string str[26]={"2","22","222",
+                    "3","33","333",
+                    "4","44","444",
+                    "5","55","555",
+                    "6","66","666",
+                    "7","77","777","7777",
+                    "8","88","888",
+                    "9","99","999","9999"
+                    };
+    string ans="";
+    for(int i=0;i<S.length();i++){
+        if(S[i]==' ')
+        ans=ans+'0';
+        else{
+            int j=S[i]-'A';
+            ans=ans+str[j];
+        }
+    }
+    return ans;
+}
