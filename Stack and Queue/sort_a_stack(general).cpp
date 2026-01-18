5bbd2dc66c5457193166979dd1360ed8aae42914
Dhiraj792 <dhirajbadre@gmail.com>
2026-01-18 20:16:46 +0530

    Add files via upload

---
+++ Stack and Queue/sort_a_stack(general).cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+stack<int>st;
+void insert_at_correct_position(int x){
+    if(st.size()==0 || x>st.top()){
+        st.push(x);
+    }
+    else{
+        int a=st.top();
+        st.pop();
+        insert_at_correct_position(x);
+        st.push(a);
+    }
+}
+void reverse(){
+    if(st.size()>0){
+        int x=st.top();
+        st.pop();
+        reverse();
+        insert_at_correct_position(x);
+    }
+}
+
+int main()
+{
+    st.push(11);
+    st.push(2);
+    st.push(32);
+    st.push(3);
+    st.push(41);
+    reverse();
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    
+
+    return 0;
+}
