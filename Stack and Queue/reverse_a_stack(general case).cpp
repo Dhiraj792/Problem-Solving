15c2509d781c71cfc88d77743b35c0315a2d8cef
Dhiraj792 <dhirajbadre@gmail.com>
2026-01-18 19:34:43 +0530

    Add files via upload

---
+++ Stack and Queue/reverse_a_stack(general case).cpp
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+stack<int>st;
+void insert_at_bottom(int x){
+    if(st.size()==0){
+        st.push(x);
+    }
+    else{
+        int a=st.top();
+        st.pop();
+        insert_at_bottom(x);
+        st.push(a);
+    }
+}
+void reverse(){
+    if(st.size()>0){
+        int x=st.top();
+        st.pop();
+        reverse();
+        insert_at_bottom(x);
+    }
+}
+
+int main()
+{
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    reverse();
+    while(!st.empty()){
+        cout<<st.top()<<" ";
+        st.pop();
+    }
+    
+
+    return 0;
+}
