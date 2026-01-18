89e627e8d7450ec4fff92e5ac6f09875a9c410e6
Dhiraj792 <dhirajbadre@gmail.com>
2026-01-18 19:51:14 +0530

    Implement stack reversal function with examples
    Added a function to reverse a stack with examples.

---
+++ Stack and Queue/reverse_a_stack_gfg.cpp
@@ -0,0 +1,42 @@
+/*
+You are given a stack st[]. You have to reverse the stack.
+
+Examples:
+
+Input: st[] = [1, 2, 3, 4]
+Output: [1, 2, 3, 4]
+Explanation: After reversing, the elements of stack are in opposite order.
+
+Input: st[] = [3, 2, 1]
+Output: [3, 2, 1]
+Explanation: After reversing, the elements of stack are in opposite order.
+
+*/
+
+
+class Solution {
+  public:
+  void insert_at_bottom(int x,stack<int>&st){
+    if(st.size()==0){
+        st.push(x);
+    }
+    else{
+        int a=st.top();
+        st.pop();
+        insert_at_bottom(x,st);
+        st.push(a);
+    }
+}
+  void reverse(stack<int>&st){
+    if(st.size()>0){
+        int x=st.top();
+        st.pop();
+        reverse(st);
+        insert_at_bottom(x,st);
+    }
+}
+    void reverseStack(stack<int> &st) {
+        reverse(st);
+        
+    }
+};
