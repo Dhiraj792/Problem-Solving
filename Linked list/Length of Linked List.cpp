f45c3bedd3f7ba490dd88094bbd2ed4d4a404cb4
Dhiraj792 <dhirajbadre@gmail.com>
2026-01-21 20:29:54 +0530

    Add function to get length of linked list
    Implemented a function to calculate the length of a singly linked list.

---
+++ Linked list/Length of Linked List.cpp
@@ -0,0 +1,43 @@
+/*
+Given head of a singly linked list. The task is to find the length of the linked list, where length is defined as the number of nodes in the linked list.
+
+Examples :
+
+Input: head : 1->2->3->4->5
+
+Output: 5
+Explanation: Length of the linked list is 5, as there 
+are 5 nodes present in it.
+Input: head : 2->4->6->7->5->1->0
+ 
+Output: 7
+Explanation: Length of the linked list is 7, as there 
+are 7 nodes present in it.
+*/
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    int getCount(Node* head) {
+        int count=0;
+        Node *ptr=head;
+        // Code here
+        while(ptr){
+            ptr=ptr->next;
+            count++;
+        }
+        return count;
+        
+    }
+};
