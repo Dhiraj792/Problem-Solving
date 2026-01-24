216f8e454fc3dab43a570ec7a33503dbcb36a651
Dhiraj792 <dhirajbadre@gmail.com>
2026-01-24 13:22:55 +0530

    Add insertAtFront function for linked list
    Implement function to insert a value at the beginning of a linked list.

---
+++ Linked list/Linked List Insertion At Beginning.cpp
@@ -0,0 +1,35 @@
+/*
+You are given the head of a Singly Linked List and a value x, insert that value x at the beginning of the LinkedList and return the head of the modified Linked List.
+
+Examples :2 -> 10
+
+Input: x = 1,
+    
+Output: 1 -> 2 -> 10
+Explanation: We can see that 1 is inserted at the beginning of the linked list.
+*/
+/*
+class Node {
+  public:
+    int data;
+    Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *insertAtFront(Node *head, int x) {
+        // Code here
+        Node *ptr=new Node(x);
+        ptr->next=head;
+        return ptr;
+        
+    }
+};
+
+    
