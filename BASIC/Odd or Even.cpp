424ad4805a7c8e6297b4eb261ac81d52da0dd215
Dhiraj792 <dhirajbadre@gmail.com>
2026-02-01 23:46:07 +0530

    Update comments for Odd or Even program

--- BASIC/Odd or Even.cpp
+++ BASIC/Odd or Even.cpp
@@ -2,7 +2,6 @@
 Given a positive integer n, determine whether it is odd or even. Return true if the number is even and false if the number is odd.
 
 Examples:
-
 Input: n = 15
 Output: false
 Explanation: The number is not divisible by 2, Odd number.
