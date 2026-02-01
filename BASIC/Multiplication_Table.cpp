22e5db431b79ec7906b251a3bb2451470ca284fe
Dhiraj792 <dhirajbadre@gmail.com>
2026-02-01 23:45:45 +0530

    Fix formatting and syntax in Multiplication_Table.cpp

--- BASIC/Multiplication_Table.cpp
+++ BASIC/Multiplication_Table.cpp
@@ -1,5 +1,5 @@
-/*Create the multiplication table from 1 to 10 for a given number n and return the table as an array.
-
+/*
+Create the multiplication table from 1 to 10 for a given number n and return the table as an array.
 Examples:
 
 Input: n = 9
@@ -7,7 +7,8 @@ Output: 9 18 27 36 45 54 63 72 81 90
 Input: n = 2
 Output: 2 4 6 8 10 12 14 16 18 20
 Constraints: 
-1 <= N <= 106*/
+1 <= N <= 106
+*/
 class Solution {
   public:
     vector<int> getTable(int n) {
@@ -19,4 +20,5 @@ class Solution {
         return table;
     }
 };
+
 
