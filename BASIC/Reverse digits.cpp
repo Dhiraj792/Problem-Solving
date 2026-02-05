5f6f7858f665cb0a0f417592a28c1aa4a3dbb32f
Dhiraj792 <dhirajbadre@gmail.com>
2026-02-05 23:51:00 +0530

    Update Reverse digits.cpp

--- BASIC/Reverse digits.cpp
+++ BASIC/Reverse digits.cpp
@@ -1,3 +1,19 @@
+/*
+Examples:
+
+Input: n = 122
+Output: 221
+Explanation: By reversing the digits of number, number will change into 221.
+Input : n = 200
+Output: 2
+Explanation: By reversing the digits of number, number will change into 2.
+Input : n = 12345 
+Output: 54321
+Explanation: By reversing the digits of number, number will change into 54321.
+Constraints:
+1 <= n <= 106
+  */
+
 class Solution {
   public:
     int reverseDigits(int n) {
