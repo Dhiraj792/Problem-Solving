c710fd3893c6ae606268cce27275eee04fa5d8f4
Dhiraj792 <dhirajbadre@gmail.com>
2026-02-05 23:50:19 +0530

    Update Reverse digits.cpp

--- BASIC/Reverse digits.cpp
+++ BASIC/Reverse digits.cpp
@@ -1,1 +1,13 @@
-
+class Solution {
+  public:
+    int reverseDigits(int n) {
+       int remainder;
+       int reversed_number=0;
+       while(n!=0){
+        remainder = n % 10;          
+        reversed_number = reversed_number * 10 + remainder; 
+        n /= 10;   
+       }
+       return reversed_number;
+    }
+};
