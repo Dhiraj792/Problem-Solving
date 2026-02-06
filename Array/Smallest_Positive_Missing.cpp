e710e7ee851c24408c03ba1d3e3d65b8886bfd03
Dhiraj792 <dhirajbadre@gmail.com>
2026-02-06 23:46:32 +0530

    Smallest_Positive_Missing.cpp

--- Array/Smallest_Positive_Missing.cpp
+++ Array/Smallest_Positive_Missing.cpp
@@ -1,6 +1,5 @@
 /*
 You are given an integer array arr[]. Your task is to find the smallest positive number missing from the array.
-
 Note: Positive number starts from 1. The array can have negative integers too.
 Input: arr[] = [2, -3, 4, 1, 1, 7]
 Output: 3
