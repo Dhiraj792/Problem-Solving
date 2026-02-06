4a3eec81c346ebc075602260401d815d379105ad
Dhiraj792 <dhirajbadre@gmail.com>
2026-02-06 23:45:43 +0530

    Array_Subset.cpp

--- Array/Array_Subset.cpp
+++ Array/Array_Subset.cpp
@@ -1,7 +1,8 @@
-//Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
-//Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
-//Output: true
-//Explanation: b[] is a subset of a[]
+/*Given two arrays a[] and b[], your task is to determine whether b[] is a subset of a[].
+Input: a[] = [11, 7, 1, 13, 21, 3, 7, 3], b[] = [11, 3, 7, 1, 7]
+Output: true
+xplanation: b[] is a subset of a[]
+*/
 class Solution {
   public:
     // Function to check if b is a subset of a
