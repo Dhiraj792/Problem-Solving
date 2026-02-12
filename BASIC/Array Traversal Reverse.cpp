2735caef46b9f8243a8a822b2bd2d7043c0a03b5
D <dhirajbadre@gmail.com>
2026-02-12 08:31:52 +0530

    Create Array Traversal Reverse.cpp

---
+++ BASIC/Array Traversal Reverse.cpp
@@ -0,0 +1,22 @@
+/*
+You are given an array arr[] that contains integers. You need to print the elements of the array with in reverse order with a in a single line with space between them.
+Note: Don't print a new line at the end.
+
+Examples:
+
+Input: arr[] = [54, 43, 2, 1, 5]
+Output: "5 1 2 43 54 "
+Explanation: Just traverse in reverse and print the numbers.
+Input: arr[] = [324, 5, 2, 2]
+Output: "2 2 5 324 "
+Explanation: Just traverse in reverse and print the numbers.
+*/
+class Solution {
+  public:
+    void arrayTraversalReverse(int numbers[], int size) {
+        // Code here
+        for(int i=size-1;i>=0;i--){
+            cout<<numbers[i]<<" ";
+        }
+    }
+};
