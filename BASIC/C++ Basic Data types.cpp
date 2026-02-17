97a213e36364f9fae7db7fa31069785d40da4080
D <dhirajbadre@gmail.com>
2026-02-17 21:22:31 +0530

    Create C++ Basic Data types.cpp

---
+++ BASIC/C++ Basic Data types.cpp
@@ -0,0 +1,73 @@
+/*
+Given a String S. Find out which of the following basic C++ data types it represents and return it's size (in bytes).
+The possible data types are:
+1. Integer
+2. Float
+3. Double
+4. Character
+
+Example 1:
+
+Input:
+S=a
+output:
+1
+Explanation:
+The string clearly represents char 
+and thus the size of char is displayed.
+Example 2:
+
+Input
+S=98.45685456
+Output: 
+8
+Explanation:
+The string represents Double.
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function BasicDataType() which takes String S as input parameter and displays the size (in bytes) of the datatype it represents. 
+*/
+
+
+class Solution {
+  public:
+    int BasicDataType(string s) {
+      if(s.size()==1&& !isdigit(s[0])){
+          return 1;
+      }
+      int size=s.size();
+       bool isdot=false;
+       int digitafterdot=0;
+       for(int i=0;i<size;i++){
+           if(s[i]=='.'){
+               isdot=true;
+               digitafterdot=size-i-1;
+               break;
+           }
+       }
+       if(isdot){
+           if(digitafterdot<6){
+               return 4;
+           }
+           else 
+           return 8;
+       }
+       return 4;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
