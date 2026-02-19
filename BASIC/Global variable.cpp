7df3b8088627d1bd6ab62ad9d299b029639b6223
D <dhirajbadre@gmail.com>
2026-02-19 16:23:27 +0530

    Initialize global variable g with value 10

---
+++ BASIC/Global variable.cpp
@@ -0,0 +1,20 @@
+/*
+You have to initialize a global variable g having int type and value as 10.
+
+Note:  This g variable is used in the main function, you don't need to change that.
+
+Example : 
+
+Input: No Input
+Output: 10
+*/
+#include <iostream>
+using namespace std;
+int g=10;
+// declare a global variable g
+
+int main() {
+    // printing the value of global variable g
+    cout << g << endl;
+
+    return 0;
