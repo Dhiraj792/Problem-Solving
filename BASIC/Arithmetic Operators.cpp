756844b14698231f439f4936760023841a08d03a
D <dhirajbadre@gmail.com>
2026-02-24 23:52:21 +0530

    Add arithmetic operations in operations function
    Implement arithmetic operations: addition, subtraction, multiplication, division, and modulo.

--- BASIC/Arithmetic Operators.cpp
+++ BASIC/Arithmetic Operators.cpp
@@ -16,3 +16,20 @@ Output: 7 -1 12 0 3
 Explanation: The given operations are performed
 
 */
+// User function Template for C++
+
+void operations(int x, int y) {
+    // Perform addition x+y below
+    int p =x+y;
+        // Perform subtraction x-y below
+        int q =x-y;
+            // Perform multiplication x*y below
+        int r =x*y;
+            // Perform division x/y below
+        int s =x/y;
+            // Perform modulo operation x%y below
+        int t =x%y;
+
+            // The below code prints the output. Don't change it!
+        cout << p << " " << q << " " << r << " " << s << " " << t << endl;
+}
