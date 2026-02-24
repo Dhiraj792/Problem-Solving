aa92f878f518761522a7231794124d2c41b87a61
D <dhirajbadre@gmail.com>
2026-02-24 23:40:11 +0530

    Add comment block for type inference example
    Add a comment block explaining type inference in C++.

---
+++ BASIC/Type Inference.cpp
@@ -0,0 +1,9 @@
+/*
+Given a C++ program having two variables a and b, it can be of any datatype like int, long, double, etc. You have to store its type id into the id1 and id2 variables.
+
+Examples:
+
+Input: a = 5, b = 's'
+Output: i c
+Explanation: typeid of int is "i" and typeid of char is "c".
+*/
