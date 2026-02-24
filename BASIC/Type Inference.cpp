f42d38816486d646a029081474c0f6f81d56999d
D <dhirajbadre@gmail.com>
2026-02-24 23:40:43 +0530

    Add typeid example for int and float variables

--- BASIC/Type Inference.cpp
+++ BASIC/Type Inference.cpp
@@ -7,3 +7,18 @@ Input: a = 5, b = 's'
 Output: i c
 Explanation: typeid of int is "i" and typeid of char is "c".
 */
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+int main() {
+    int a = 3;
+    float b = 5.57;
+
+    // calculate typeid for variable a and b in variables id1 and id2.
+    string id1 = typeid(a).name();
+    string id2 = typeid(b).name();
+
+    // printing the typeid's
+    cout << id1 << " " << id2 << endl;
+}
