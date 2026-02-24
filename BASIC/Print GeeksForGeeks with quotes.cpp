02e6945bb7543cdb2dea47179ab893f34dd9c6c9
D <dhirajbadre@gmail.com>
2026-02-24 23:45:42 +0530

    Create Print GeeksForGeeks with quotes.cpp
    Add a new C++ file to print 'GeeksForGeeks' with quotes

---
+++ BASIC/Print GeeksForGeeks with quotes.cpp
@@ -0,0 +1,9 @@
+/*
+You are given a print() function that should output one statement as "GeeksForGeeks". The statement should be with double quotes as shown.
+
+Example : 
+
+Input: Not Required
+Output: "GeeksForGeeks"
+
+*/
