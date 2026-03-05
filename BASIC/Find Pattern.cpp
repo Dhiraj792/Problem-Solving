a49b941c0cffebd812b339266294a67329562f4c
D <dhirajbadre@gmail.com>
2026-03-05 22:48:05 +0530

    Update Find Pattern.cpp

--- BASIC/Find Pattern.cpp
+++ BASIC/Find Pattern.cpp
@@ -3,7 +3,6 @@ Given a string s, and a pattern p. You need to find if p exists in s or not and 
 Here p and s both are case-sensitive.
 
 Examples:
-
 Input: s = "Hello", p = "llo"
 Output: 2
 Explanation: llo starts from the second index in Hello.
