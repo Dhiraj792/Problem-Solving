db13fe9db81f489e682f9008adaf5e5dfdc1ec73
D <dhirajbadre@gmail.com>
2026-04-27 23:41:14 +0530

    Update comment for middle element of linked list
    Added a comment explaining the function's behavior for linked lists with two middle nodes.

--- Linked list/Middle_element_of_linked_list.cpp
+++ Linked list/Middle_element_of_linked_list.cpp
@@ -4,7 +4,6 @@ Given the head of a singly linked list, return the middle node of the linked lis
 If there are two middle nodes, return the second middle node.
 Example 1:
 
-
 Input: head = [1,2,3,4,5]
 Output: [3,4,5]
 Explanation: The middle node of the list is node 3.
