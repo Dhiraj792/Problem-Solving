f052b093bfee78ce3d6cc18c9078158cee94e53c
D <dhirajbadre@gmail.com>
2026-06-20 23:20:30 +0530

    Update Kth Smallest.cpp

--- Array/Kth Smallest.cpp
+++ Array/Kth Smallest.cpp
@@ -5,7 +5,6 @@ Given an integer array arr[] and an integer k, find and return the kth smallest 
 Note: The kth smallest element is determined based on the sorted order of the array.
 
 Examples :
-
 Input: arr[] = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10], k = 4
 Output: 5
 Explanation: 4th smallest element in the given array is 5.
