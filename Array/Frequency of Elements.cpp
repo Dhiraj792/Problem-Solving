ccd09bd8176f21d8b2a9298f978dee96505d1838
D <dhirajbadre@gmail.com>
2026-07-01 23:49:03 +0530

    Update Frequency of Elements.cpp

--- Array/Frequency of Elements.cpp
+++ Array/Frequency of Elements.cpp
@@ -1,9 +1,7 @@
 /*Frequency of Elements
 Difficulty: EasyAccuracy: 57.13%Submissions: 15K+Points: 2
 Given an array arr[] of positive integers which may contain duplicate elements, return the frequency of each distinct element.
-
 Examples:
-
 Input: arr[] = [1, 2, 2, 3, 3, 5]
 Output: [[1, 1], [2, 2], [3, 2], [5, 1]]
 Explaiantion: Here element 1 and 5 occur 1 times, 2 and 3 occur 2 times.
